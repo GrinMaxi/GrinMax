8d49afd83d9872c0dd3c3de9771efffebc65bde8
GrinMaxi <grin2181@xmail.ru>
2024-06-30 12:45:31 +0500

    First commit

---
+++ go/src/stepic-go-basic/1_3_1/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Println("Java")
+	// fmt.Println("Swift")
+	fmt.Println("Go") // fmt.Println("C++")
+	// fmt.Println("C#")
+	fmt.Println("Python") // fmt.Println("JavaScript")
+	fmt.Println("Здравствуй,\nИосиф!")
+	fmt.Println("other")
+	// end
+	fmt.Println("row1")
+	fmt.Print("row2")
+	fmt.Println("row3")
+	fmt.Print("row4")
+}
